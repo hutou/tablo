441b1cc69d96ea4a48654c330226fdcf0928d44c
hutou <hutou01@gmail.com>
2023-10-09 22:15:52 +0200

    Obey 'omit_last_rule' for table summary

--- tests/z16.cr
+++ tests/z16.cr
@@ -2,10 +2,12 @@ require "tablo"
 require "debug"
 
 table = Tablo::Table.new([17.0, -42, nil, -74, 15, "abc", false, "bca"],
+  # border_type: :fancy,
   omit_last_rule: true) do |t|
   t.add_column("itself", &.itself)
   t.add_column("triple") { |n| n.nil? ? nil : (n.is_a?(Bool) ? n : n * 3) }
-  t.add_column("Double") { |n| n.nil? ? nil : (n.is_a?(Bool) ? n : n * 2) }
+  t.add_column("Double",
+    body_formatter: ->(c : Tablo::CellType) { c.is_a?(String) ? c.as(String).upcase : c.to_s }) { |n| n.nil? ? nil : (n.is_a?(Bool) ? n : n * 2) }
   t.summary({
     "itself" => {
       header: "My sum",
@@ -36,7 +38,28 @@ table = Tablo::Table.new([17.0, -42, nil, -74, 15, "abc", false, "bca"],
       ],
     },
   },
-    masked_headers: false)
+    border_type: "+++++++++SSS----",
+    omit_last_rule: true,
+    masked_headers: true)
 end
 puts table
 puts table.summary
+# puts
+# ts = table.summary.as(Tablo::Table(Array(Tablo::CellType)))
+# ts.each_with_index do |row, i|
+#   if i == 0
+#     lines = row.to_s.split("\n")
+#     # puts lines[-2]
+#     puts lines[-1]
+#     # puts lines[-2]
+#   end
+# end
+puts
+table.each do |row|
+  row.each { |cell| puts cell.value }                    # 1, false, true...2, true, false...3, false, true
+  row.each { |cell| puts cell.memoized_formatted_value } # 1, false, true...2, true, false...3, false, true
+  # puts row.to_h["triple"].value       # false...true...false
+  p! row.to_h
+  p! row.to_h["triple"] # false...true...false
+  puts
+end
